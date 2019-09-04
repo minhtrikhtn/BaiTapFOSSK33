33669cc89bf04fb8f9cdfb69b4a0380d690f5f4a
minhtrikhtn <54771007+minhtrikhtn@users.noreply.github.com>
2019-09-04 08:51:29 +0700

    Add files via upload

---
+++ ChuanHoaDuLieuNN/ChuanHoaDuLieuNN/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace ChuanHoaDuLieuNN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChonFileKQ_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChuanhoa_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
